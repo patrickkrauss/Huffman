8b6ca0d483509389e20c0a1d4d84e6f1a324abaa
gustavo henrique spiess <gustavospiess@gmail.com>
2018-06-19 21:06:03 -0300

    Correção dos nomes e packages;

--- Huffman/src/huffman/HuffmanDecompacter.java
+++ Huffman/src/huffman/HuffmanDecompacter.java
@@ -1,11 +1,11 @@
-package br.furb;
+package huffman;
 
-import br.furb.estruturas.ListaEncadeada.Generica.ListaEncadeada;
-import br.furb.estruturas.ListaEncadeada.Generica.NoLista;
-import br.furb.estruturas.arvores.ArvoreBinaria.ArvoreBinaria;
-import br.furb.estruturas.arvores.ArvoreBinaria.NoArvoreBinaria;
-import br.furb.interfaces.IHuffmanChar;
-import br.furb.interfaces.TextFile;
+import huffman.estruturas.ListaEncadeada.Generica.ListaEncadeada;
+import huffman.estruturas.ListaEncadeada.Generica.NoLista;
+import huffman.estruturas.arvores.ArvoreBinaria.ArvoreBinaria;
+import huffman.estruturas.arvores.ArvoreBinaria.NoArvoreBinaria;
+import huffman.interfaces.IHuffmanChar;
+import huffman.interfaces.TextFile;
 
 public class HuffmanDecompacter {
 
--- Huffman/src/huffman/HuffmanUtils.java
+++ Huffman/src/huffman/HuffmanUtils.java
@@ -1,27 +1,14 @@
-package br.furb;
+package huffman;
 
 
-import br.furb.estruturas.ListaEncadeada.Generica.ListaEncadeada;
-import br.furb.estruturas.ListaEncadeada.Generica.NoLista;
-import br.furb.estruturas.arvores.ArvoreBinaria.NoArvoreBinaria;
-import br.furb.interfaces.IHuffmanChar;
+import huffman.estruturas.ListaEncadeada.Generica.ListaEncadeada;
+import huffman.estruturas.ListaEncadeada.Generica.NoLista;
 
 import java.util.Comparator;
 
 public class HuffmanUtils {
 
     public static  <T> void sort(ListaEncadeada<T> caracteres, Comparator<T> comparator) {
-        /*NoLista<T> atual = caracteres.getPrimeiro();
-        while (atual.getProximo() != null) {
-            NoLista<T> proximo = atual.getProximo();
-            if (comparator.compare(proximo.getInfo(), atual.getInfo()) > 0) {
-                atual.setProximo(proximo.getProximo());
-                proximo.setProximo(caracteres.getPrimeiro());
-                caracteres.setPrimeiro(proximo);
-            }
-            atual = proximo;
-        }*/
-
         int size = caracteres.obterComprimento();
         if (size > 1) {
             for (int i = 0; i < size; i++ ) {
